6d32a6fc5f393e5bc71ce5cb57dd27fdb1ac518d
shandohowell <shandohowell94@gmail.com>
2025-01-04 12:43:48 -0500

    Worked on creating the core UI layout.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const HomePage = () => {
 
   return (
-    <></>
+    <>
+      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4}/>
+    </>
   )
 }
 
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+
+const ProductPrice = ({ value, className }: { value: number; className?: string; }) => {
+    // Ensure tow decimal places
+    const stringValue = value.toFixed(2);
+
+    // Get the int/float
+    const [intValue, floatValue] = stringValue.split('.')
+
+    return (
+        <p className={ cn('text-2xl', className)}>
+            <span className="text-xs align-super">$</span>
+            {intValue}
+            <span className="text-xs align-super">.{ floatValue }</span>
+        </p>
+    )
+}
+
+export default ProductPrice
