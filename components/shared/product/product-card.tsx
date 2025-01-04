02d6be3cc02e165657d8e374584d0837450ac0dc
shandohowell <shandohowell94@gmail.com>
2025-01-04 16:48:24 -0500

    Implemented database seed, zod validation and product actions.

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
 import ProductPrice from "./product-price";
+import { Product } from "@/types";
 
-const ProductCard = ({ product }: { product: any}) => {
+const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
         <CardHeader className="p-0 items-center">
@@ -38,4 +39,4 @@ const ProductCard = ({ product }: { product: any}) => {
   )
 }
 
-export default ProductCard
+export default ProductCard;
